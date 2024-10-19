ed1f55b40b21bad58c970a606f99e224cb8d8e8f
Pedro <psuar92@gmail.com>
2024-10-19 14:38:19 -0500

    create date validator

---
+++ helpers/isDate.js
@@ -0,0 +1,14 @@
+const { isValid } = require('date-fns');
+
+const isDate = (value) => {
+
+    if(!value) return false;
+
+    const date = isValid(value);
+
+    return date;
+}
+
+module.exports = {
+    isDate
+}
